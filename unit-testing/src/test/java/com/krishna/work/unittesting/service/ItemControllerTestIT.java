1b97be4d219301f585f172b95919ad340ad6dfbc
krishna Chavhan <krishna.chavhan@cognizant.com>
2019-03-17 16:15:32 +0530

    Update ItemControllerTestIT.java

--- unit-testing/src/test/java/com/krishna/work/unittesting/service/ItemControllerTestIT.java
+++ unit-testing/src/test/java/com/krishna/work/unittesting/service/ItemControllerTestIT.java
@@ -7,10 +7,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.krishna.work.unittesting.dao.ItemDao;
 import com.krishna.work.unittesting.module.Item;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -22,6 +25,10 @@ public class ItemControllerTestIT {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	
+	@MockBean
+	private ItemDao itemDao;
+	
 	@Test
 	public void testCase() {
 		logger.info("Start:--------ItemControllerTestIT::testCase---------");
@@ -35,6 +42,8 @@ public class ItemControllerTestIT {
 	@Test
 	public void getItemFromServiceTest() {
 		logger.info("Start:--------ItemControllerTestIT::getItemFromServiceTest---------");
+		when(itemDao.getItem()).thenReturn(new Item(110, "SpringBook", 555.0, 5));
+		
 		Item item = (Item) this.restTemplate.getForObject("/getItemFromService",
 				Item.class);
 		logger.info("item=>\t" + item);
