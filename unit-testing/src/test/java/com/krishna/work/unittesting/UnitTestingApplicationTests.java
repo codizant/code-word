6b8bc56c2e82db900dfe953d4107f301b57d7907
VEDANT <krish.chavhan@gmail.com>
2020-07-03 19:41:44 +0530

    //test add to git

--- unit-testing/src/test/java/com/krishna/work/unittesting/UnitTestingApplicationTests.java
+++ unit-testing/src/test/java/com/krishna/work/unittesting/UnitTestingApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UnitTestingApplicationTests {
-
+//Test add to git
 	@Test
 	public void contextLoads() {
 	}
